acba2acd3c665b7f5572148e8492886de7aafc0e
Moni <monica_be@hotmail.fr>
2020-07-26 12:00:19 +0200

    setting css and awesome font

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,20 +5,21 @@ const Home = () => {
   return (
     <div className='container-global'>
       <div className='header'>
-        <img src='#' alt='logo' className='logo' />
+        <h1 className='signature'>Idées d'antan</h1>
+        {/* <img src='#' alt='logo' className='logo' /> */}
         <nav className='nav'>
           <ul>
-            <li><a href='#'>contact</a></li>
-            <li><a href='#'>newsletter</a></li>
+            <li className='links-nav'><a href='#'>contact</a></li>
+            <li className='links-nav'><a href='#'>newsletter</a></li>
           </ul>
         </nav>
       </div>
       <div className='submenu'>
-        <h3>Idées d'antan</h3>
+        <h3 className='signature'>Notre catalogue</h3>
         <nav className='nav'>
           <ul>
-            <li><a href='#'>livres</a></li>
-            <li><a href='#'>revues</a></li>
+            <li className='links-nav'><a href='#'>livres</a></li>
+            <li className='links-nav'><a href='#'>revues</a></li>
           </ul>
         </nav>
       </div>
