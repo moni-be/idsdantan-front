d7947e189e57f723b79d43ac6a521b496ce5d7a9
Moni <monica_be@hotmail.fr>
2020-10-13 00:49:21 +0200

    setting categorie in livre page

--- src/Pages/Home/CatalogueMenu.js
+++ src/Pages/Home/CatalogueMenu.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { NavLink, useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import onClickOutside from 'react-onclickoutside'
 import axios from 'axios'
 import CategoryBook from './CategoryBook'
 
 import './../GlobalDantan.css'
+import { render } from '@testing-library/react'
 
 function CatalogueMenu (props) {
   const { categoryId } = useParams()
@@ -17,29 +18,44 @@ function CatalogueMenu (props) {
   /*   function handleOnClick(item){
   } */
 
-  const handleChange = (e) => {
-    setCategory({ category: categoryId })
-  }
-
   useEffect(() => {
     showAllCategory()
   }, [])
 
   const showAllCategory = () => {
-    axios.get(`http://localhost:4000/categorie-livre/${category}`)
+    axios.get(`http://localhost:4000/livre/categorie/${category}`)
       .then(res => setCategory(res.data))
   }
 
   return (
     <div className='dropdown'>
+      {category.map((category, key) => {
+        return <Link to={`/categorie-livre/${category}`} className='links-nav' key={key}>
+          <CategoryBook key={category.id} category={category} />
+        </Link>
+      })}
+
+      {/*
+
+    const handleChange = (value) => {
+      const currentIndex = category.indexOf(value)
+      const newCategory = [...category]
+
+      if (currentIndex === -1) {
+        newCategory.push(value)
+      } else {
+        newCategory.splice(currentIndex, 1)
+      }
+      setCategory(newCategory)
+    }
       <a href='#' onKeyPress={() => toggle(!open)} onClick={() => toggle(!open)}> <span className='catalogue'>Notre catalogue</span></a>
       {open && (
         <ul>
-          <li>
-            <CategoryBook onClick={handleChange} className='links-nav' />
+          <li> <NavLink to='/livre/conte' className='links-nav' onClick={handleOnChange}>Conte</NavLink>
+          </li>
+          <li> <NavLink to='/livre/roman' className='links-nav' onClick={handleOnChange}>Roman</NavLink>
           </li>
 
-          {/*
                     {category.name}
             {category.map((category, key) => {
               return <CategoryBook isCategory={0} category={category} key={key} />
@@ -51,17 +67,20 @@ function CatalogueMenu (props) {
           <li>
             <NavLink to='/livre/roman' className='links-nav' onClick={handleChange}>Roman</NavLink>
           </li>
-      */}
+
         </ul>
       )}
+       */}
     </div>
   )
 }
 
-// export default CatalogueMenu
+export default CatalogueMenu
 
+/*
 const clickOutsideConfig = {
   handleClickOutside: () => CatalogueMenu.handleClickOutside
 }
 
 export default onClickOutside(CatalogueMenu, clickOutsideConfig)
+*/
--- src/Pages/Home/CategoryBook.js
+++ src/Pages/Home/CategoryBook.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './../GlobalDantan.css'
 
 const CategoryBook = (props) => {
   const { category } = props
   if (props.category) {
     return (
-      <span className='links-nav'>{category.name}</span>
+    /* <Link to={`/categorie-livre/${category}`} className='links-nav'>
+        <span>{category.name}</span>
+      </Link> */
+      <span>{category.name}</span>
     )
   }
 }
