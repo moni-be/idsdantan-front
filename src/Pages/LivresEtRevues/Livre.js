cb43e15c65c7a3c8004485a2f77596d9476dd57b
Moni <monica_be@hotmail.fr>
2020-11-18 19:12:02 +0100

    changing Livre and category code

--- src/Pages/LivresEtRevues/Livre.js
+++ src/Pages/LivresEtRevues/Livre.js
@@ -9,44 +9,51 @@ import MenuButton from './MenuButton'
 import '../GlobalDantan.css'
 import './Livre&Revue.css'
 
-const Livre = (props) => {
+const Livre = () => {
   const [book, setBook] = useState([])
   // const { categoryId } = useParams()
-  // const [bookCategory, setBookCategory] = useState([])
+  const [bookCategory, setBookCategory] = useState([])
+  const [category, setCategory] = useState(false)
 
   useEffect(() => {
     showAllBooks()
   }, [])
 
+  /*  useEffect(() => {
+    showBookCategory()
+  },
+  [bookCategory]) */
+
   const showAllBooks = () => {
     axios.get(`http://localhost:4000/livre/${book}`)
-      .then(res => setBook(res.data))
+      .then(res => setBook(res.data), console.log(res => setBookCategory(res.data)))
   }
 
-  const handleCategory = (e) => {
-    if (book === 0) {
-      book.id = setBook
-    } else {
-      book.filter(book => book.category_id)
-    }
-  }
-
-  /*   useEffect(() => {
-    showBookCategory()
-  }, [])
-
-  const showBookCategory = () => {
-    axios.get(`http://localhost:4000/livre/categorie/${categoryId}`)
-      .then(res => setBookCategory(res.data))
+  /*  const showBookCategory = () => {
+    axios.get(`http://localhost:4000/livre/categorie/${bookCategory}`)
+      .then(
+        console.log(res => setBookCategory(res.data))
+      )
   } */
+
+  const getByCategory = () => {
+    bookCategory({ category: !setCategory }, () => {
+      let bookTemp
+      if (category) {
+        bookTemp = book.filter(book => book.category_id)
+      } else {
+        bookTemp = setBook
+      }
+      bookCategory({ setBookCategory: bookTemp })
+    })
+  }
 
   return (
     <div className='container-global'>
       <Header />
       <div className='container-livres'>
         {/* <h1 className='title'>Livres</h1> */}
-
-        <MenuButton onClick={handleCategory} />
+        <MenuButton onClick={getByCategory} />
 
         <div className='container-vignettes'>
           {book.map((book, key) => {
--- src/Pages/LivresEtRevues/MenuButton.js
+++ src/Pages/LivresEtRevues/MenuButton.js
@@ -18,7 +18,7 @@ function MenuButton () {
     <div className='container-buttons'>
       {category.map((category, key) => {
         return (
-          <button className='menu-buttons' key={key}>
+          <button type='button' className='menu-buttons' key={key}>
             {category.name}
           </button>
         )
