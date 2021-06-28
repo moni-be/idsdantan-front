54dda6a8eec081bb50f2de9dc64a291c397fa609
Moni <monica_be@hotmail.fr>
2021-06-28 18:11:40 +0200

    erasing syntax error

--- src/Pages/LivresEtRevues/ModelRevue.js
+++ src/Pages/LivresEtRevues/ModelRevue.js
@@ -14,22 +14,20 @@ const ModelRevue = (props) => {
 
   if (props.revue) {
     return (
-      <>
-        <figure className='vignette'>
+      <figure className='vignette'>
+        <Link to={`/ma-revue/${revue.id}`}>
+          <img src={`/assets/revues/${revue.image_name}`} className='photo' alt={revue.title} />
+        </Link>
+        <figcaption className='texte'>
+          <h3>{revue.title} </h3>
+          <h4>Numéro : {revue.number}</h4>
+          <h5>Prix : {revue.price}&euro;</h5>
           <Link to={`/ma-revue/${revue.id}`}>
-            <img src={`/assets/revues/${revue.image_name}`} className='photo' alt={revue.title} />
+            <FontAwesomeIcon icon={faBookmark} className='icon-buy' />
+            <span className='icon-txt' onClick={handleChange}>Je réserve</span>
           </Link>
-          <figcaption className='texte'>
-            <h3>{revue.title} </h3>
-            <h4>Numéro : {revue.number}</h4>
-            <h5>Prix : {revue.price}&euro;</h5>
-            <Link to={`/ma-revue/${revue.id}`}>
-              <FontAwesomeIcon icon={faBookmark} className='icon-buy' />
-              <span className='icon-txt' onClick={handleChange}>Je réserve</span>
-            </Link>
-          </figcaption>
-        </figure>
-      </>
+        </figcaption>
+      </figure>
     )
   }
 }
